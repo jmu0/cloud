d6f6c89222be8ed1efd0e27a0095afcf8804d25f
Jos Muysers <jos@muysers.nl>
2015-06-10 23:55:11 +0200

    test socket client

--- test.socket.client.py
+++ test.socket.client.py
@@ -2,7 +2,10 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('10.0.0.5',7777))
 s.send(str.encode('data'))
-result = s.recv(4092)
-result.decode()
-print(result)
-
+data = ""
+while True:
+    buf =  s.recv(1024)
+    data += buf
+    if not buf or buf.find('\n'): break
+data = data.decode()
+print(data)
--- test.socket.server.py
+++ test.socket.server.py
@@ -1,8 +1,7 @@
 import socket
 import sys
-# import threading
 
-
+#setup socket
 host = '10.0.0.5'
 port = 7777 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,34 +10,24 @@ try:
     s.bind((host, port))
 except socket.error as e:
     print(str(e))
-s.listen(5)
+s.listen(256)
 
-# lock = threading.Lock()
+#command function
+def doCommand(cmd):
+    print(cmd)
+    if cmd[0] == 'data':
+        return "hier is de data\n"
 
-def threaded_client(conn):
-    conn.send(str.encode('Welcome, type your info \n'))
-
-    while True: 
-        data = conn.recv(2046)
-        # with lock:
-        print "Received: " + data.decode()
-        reply = "Server output: " + data.decode()
-        if not data:
-            # with lock:
-            print('Disconnected')
-            break
-        conn.sendall(str(reply).encode())
-    conn.close()
-
-
+#endless loop
 while True:
     conn, addr = s.accept()
     print('connected to: ' + addr[0] + ":" + str(addr[1]))
-    data = conn.recv(4092)
-    cmd = data.decode().split()
-    print(cmd)
-    if cmd[0] == 'data':
-        conn.sendall(str.encode('hier is de data\n'))
+    data = ""
+    while True:
+        buf =  conn.recv(1024)
+        data += buf
+        if not buf or buf.find('\n'): break
+    data = data.decode()
+    cmd = data.split()
+    conn.sendall(str(doCommand(cmd)).encode())
     conn.close()
-
-
