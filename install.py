bb83f917d3ff8ba380f8717890bdb70ef8287c7c
Jos Muysers <jos@muysers.nl>
2015-06-19 11:22:12 +0200

    started initscripts

---
+++ install.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# TODO: copy scripts to /usr/share/cloud
+# TODO: link cloud.py in /usr/bin
+# TODO: probe which distribution (sysvinit / upstart / systemd)
+# TODO: copy startup script
+
+# startup: /etc/init (ubuntu ...)
+# systemd: (arch ...)
+# sysvinit: /etc/init.d (debian ...)
+
+
