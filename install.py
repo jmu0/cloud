e3a5c4d26755882485e05205a4d223a8bd9511bb
Jos Muysers <jos@muysers.nl>
2015-06-21 17:47:37 +0200

    systemd service initscript

--- install.py
+++ install.py
@@ -8,11 +8,14 @@
 # startup: /etc/init (ubuntu ...)
 
 # systemd: (arch ...)
+#   copy to /etc/systemd/system
+#   sudo systemclt enable cloud.service
+#   sudo systemctl start cloud.service
 
 # sysvinit: /etc/init.d (debian ...)
 #   copy to /etc/init.d
 #   ????  sudo update-rc.d /etc/init.d/cloud defaults
 #   sudo update-rc.d /etc/init.d/cloud enable
-#   sudo /etc/init.d/cloud start 
+#   sudo /etc/init.d/cloud start
 
 
